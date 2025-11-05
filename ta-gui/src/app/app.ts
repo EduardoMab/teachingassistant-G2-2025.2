66d45434f6dfb5b7f9a68cb711868e039f75d3d7
Eduardo Mabesoone <emm4@cin.ufpe.br>
2025-11-04 21:37:03 -0300

    Alterações questão 26

--- ta-gui/src/app/app.ts
+++ ta-gui/src/app/app.ts
@@ -13,8 +13,10 @@ export class App {
   protected readonly title = signal('Cadastro de Alunos');
   aluno: Aluno = { nome: '', cpf: '', email: '', github: '' };
   alunoService = new AlunoService();
+  alunos: Aluno[] = [];
    gravar(a: Aluno): void {
     this.alunoService.gravar(a);
+    this.alunos.push(a);
     this.aluno = { nome: '', cpf: '', email: '' , github: ''};
   }
 }
